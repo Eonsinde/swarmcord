2393822d44e9a45042519d64eec49cbea1bb9a7d
eonsinde <olasinde.eon@gmail.com>
2025-11-23 10:21:42 +0100

    removed convresations fetching

--- src/app/(site)/(me)/layout.tsx
+++ src/app/(site)/(me)/layout.tsx
@@ -1,24 +1,21 @@
 import { redirectToSignIn } from "@clerk/nextjs"
-import { currentProfileWithMembers } from "@/lib/current-profile"
+import { currentProfile } from "@/lib/current-profile"
 import MeSidebar from "@/components/me/me-sidebar"
-import { getConversations } from "@/lib/conversation"
 
 const MeLayout = async ({
     children
 }: {
     children: React.ReactNode
 }) => {
-    const profile = await currentProfileWithMembers();
+    const profile = await currentProfile();
 
     if (!profile)
         return redirectToSignIn();
 
-    const conversations = await getConversations(profile.members.map((member => member.id)));
-
     return (
         <div className="h-full">
             <div className="fixed inset-y-0 z-20 hidden md:flex h-full w-[300px] flex-col">
-                <MeSidebar conversations={conversations!} />
+                <MeSidebar currentProfile={profile} />
             </div>
             <main className="h-full md:pl-[300px]">
                 {children}
