a6633d96ea890919b704fa65f1a46b3c5757bcc5
eonsinde <olasinde.eon@gmail.com>
2025-11-23 15:59:34 +0100

    handles rerouting to default channel for a server

--- src/app/(site)/servers/[serverId]/page.tsx
+++ src/app/(site)/servers/[serverId]/page.tsx
@@ -1,8 +1,45 @@
+import { redirect, notFound } from "next/navigation"
+import { redirectToSignIn } from "@clerk/nextjs"
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
 
-const ServerIdPage = () => {
-    return (
-        <></>
-    )
+type Props = {
+    params: {
+        serverId: string
+    }
+}
+
+const ServerIdPage = async ({ params }: Props) => {
+    // if a request is made to this route, redirect user to the default channel
+    // for the given server ID or a not-found page if channel isn't valid
+    const profile = await currentProfile();
+
+    if (!profile)
+        return redirectToSignIn();
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId
+        },
+        include: {
+            channels: {
+                where: {
+                    default: true
+                },
+                take: 1
+            }
+        }
+    });
+
+    if (!server)
+        return notFound();
+
+    const defaultChannel = server?.channels[0];
+
+    if (!defaultChannel)
+        return null;
+
+    return redirect(`/servers/${params.serverId}/${defaultChannel.id}`);
 }
  
 export default ServerIdPage
