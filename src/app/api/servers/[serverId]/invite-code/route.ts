076a74d7f5cbd142874758a9ec017dbb0c609e7a
eonsinde <olasinde.eon@gmail.com>
2025-11-23 16:00:08 +0100

    added comments for file clarity

--- src/app/api/servers/[serverId]/invite-code/route.ts
+++ src/app/api/servers/[serverId]/invite-code/route.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid"
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 
+// this is to update the invite code for a server
 export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
     try {
         const profile = await currentProfile();
